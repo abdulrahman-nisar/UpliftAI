e3e3a42009199083feaa7f03b35ca1f101f9adec
danialzaheerfastian@outlook.com <danialzaheerfastian@outlook.com>
2025-10-25 00:26:08 +0500

    Deleted all falto things in code and started from 0.

--- app.py
+++ app.py
@@ -23,55 +23,6 @@ def login():
 
     return render_template('index.html', firebase_api_key=firebase_api_key)
 
-    
-# @app.route('/<int:user_id>/entries', methods=['GET'])
-# def get_all_entries(user_id: int):
-#     entries = journal_services.get_all_journal_entires(user_id)
-#     return render_template('frontend/index.html', entries=entries, user_id=user_id)
-
-
-# @app.route('/<int:user_id>/entries/<int:entry_id>', methods=['GET'])
-# def get_entry(user_id: int, entry_id: int):
-#     entry = journal_services.get_journal_entry(entry_id)
-#     return render_template('frontend/edit_entry.html', entry=entry, user_id=user_id)
-
-
-# @app.route('/<int:user_id>/entries', methods=['POST'])
-# def create_entry(user_id: int):
-#     data = request.form
-
-#     new_entry = JournalEntry(
-#         user_id=user_id,
-#         entry_id=data.get('entry_id'),
-#         title=data.get('title'),
-#         content=data.get('content'),
-#         date=date.today()
-#     )
-    
-#     journal_services.create_journal_entry(new_entry)
-#     return redirect(url_for('get_all_entries', user_id=user_id))
-
-
-# @app.route('/<int:user_id>/entries/<int:entry_id>', methods=['POST'])
-# def update_entry(user_id: int, entry_id: int):
-#     data = request.form
-
-#     entry = JournalEntry(
-#         user_id=user_id,
-#         entry_id=data.get('entry_id'),
-#         title=data.get('title'),
-#         content=data.get('content'),
-#         date=date.today()
-#     )
-    
-#     journal_services.update_journal_entry(entry)
-#     return redirect(url_for('get_all_entries', user_id=user_id))
-
-
-# @app.route('/<int:user_id>/entries/<int:entry_id>/delete', methods=['POST'])
-# def delete_entry(user_id: int, entry_id: int):
-#     journal_services.delete_journal_entry(entry_id)
-#     return redirect(url_for('get_all_entries', user_id=user_id))
 
 if __name__ == "__main__":
     app.run(debug=True)
--- models/UserModel.py
+++ models/UserModel.py
@@ -1,5 +1,6 @@
-class UserModel:
-    def __init__(self, user_name, password):
-        self.user_name = user_name
-        self.password = password
-        
+class User:
+    def __init__(self, user_id, name, email, password_hash):
+        self.user_id = user_id
+        self.name = name
+        self.email = email
+        self.password_hash = password_hash
